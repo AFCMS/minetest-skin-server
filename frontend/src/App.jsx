134aee347b26bcd834add769c114b376f7b7ece7
AFCMS <afcm.contact@gmail.com>
2022-12-25 23:48:33 +0100

    [frontend] Organise CSS + add About page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
-import "./App.css";
+import "./style/buttons.css";
 
 function App() {
 	return (
-		<div className="App">
-			<Header />
+		<div className="bg-blue-200 min-h-screen">
 			<BrowserRouter>
+				<Header />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
+					<Route path="/about" element={<About />} />
 					<Route path="/*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
---
+++ frontend/src/pages/About/index.jsx
@@ -0,0 +1,5 @@
+function About() {
+	return <div className="">About Page</div>;
+}
+
+export default About;
