21cdadf21f76db67cf2cb59989a2fee00cc48732
AFCMS <afcm.contact@gmail.com>
2022-12-27 14:53:39 +0100

    [frontend] Add some basic validation to login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [canSubmit, setCanSubmit] = useState(false);
+
+	useEffect(() => {
+		if (email === "" && password === "") {
+			setCanSubmit(false);
+		} else {
+			setCanSubmit(true);
+		}
+	}, [email, password, setCanSubmit]);
+
+	const navigate = useNavigate();
+
 	return (
 		<div className="justify-center align-middle flex">
 			<div className="flex flex-col max-w-md p-6 gap-4 m-10 app-pannel">
@@ -11,6 +29,10 @@ function Login() {
 						id="email"
 						placeholder="Ex: someone@gmail.com"
 						className="form"
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
 					/>
 				</label>
 				<label className="block">
@@ -21,13 +43,27 @@ function Login() {
 						id="password"
 						placeholder="SuperStrongPassword"
 						className="form"
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
 					/>
 				</label>
 				<div className="flex gap-4 mt-4">
-					<button className="button-secondary w-full">
+					<button
+						className="button-secondary w-full"
+						onClick={(e) => {
+							navigate("/register");
+						}}
+					>
 						Register
 					</button>
-					<button className="button-primary w-full">Login</button>
+					<button
+						className="button-primary w-full disabled:bg-black disabled:cursor-not-allowed"
+						disabled={!canSubmit}
+					>
+						Login
+					</button>
 				</div>
 			</div>
 		</div>
