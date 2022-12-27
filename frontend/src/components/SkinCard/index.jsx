38167268518bda789869a8b89a92e573943fced8
AFCMS <afcm.contact@gmail.com>
2022-12-27 21:54:10 +0100

    [frontend] Improved skin card

--- frontend/src/components/SkinCard/index.jsx
+++ frontend/src/components/SkinCard/index.jsx
@@ -1,19 +1,27 @@
-import { HeartIcon, Bars3Icon } from "@heroicons/react/24/solid";
+import {
+	HeartIcon,
+	Bars3Icon,
+	FolderArrowDownIcon,
+	DocumentArrowDownIcon,
+} from "@heroicons/react/24/solid";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import PropTypes from "prop-types";
 
 /**
  * Represent a skin to be displayed in a grid.
+ * @param {{description: string}} param0
  */
 function SkinCard({ description }) {
 	return (
 		<div className="rounded h-72 w-56 border shadow shadow-slate-500 bg-blue-100">
 			<div className="h-3/4">Skin Here</div>
-			<div className="h-1/4 px-4 py-2 border-t border-t-slate-500">
-				<h2 className="font-bold text-clip text-lg">{description}</h2>
-				<h3>by AFCM</h3>
-				<div className="top-2 static right-2 flex align-baseline items-end">
+			<div className="h-1/4 px-4 py-2 border-t border-t-slate-500 relative">
+				<h2 className="font-bold text-clip text-lg text-slate-800">
+					{description}
+				</h2>
+				<h3 className="text-slate-800">by AFCM</h3>
+				<div className="bottom-2 absolute right-2 flex align-baseline items-end">
 					<button aria-label="like skin" className="h-8 w-8">
 						<HeartIcon className="text-red-500" />
 					</button>
@@ -25,7 +33,7 @@ function SkinCard({ description }) {
 								aria-haspopup="listbox"
 							>
 								<span className="sr-only">Skin Options</span>
-								<Bars3Icon className="" />
+								<Bars3Icon className="text-slate-800" />
 							</Menu.Button>
 						</div>
 						<Transition
@@ -39,13 +47,35 @@ function SkinCard({ description }) {
 						>
 							<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 								<Menu.Item>
-									<a
-										href={"google.com"}
+									<span
 										className={
-											"block px-4 py-2 text-sm text-gray-700"
+											"flex flex-row align-baseline h-8 p-1 gap-2 text-slate-800 text-sm px-2 hover:bg-slate-200"
 										}
 									>
-										{"Download Skin"}
+										<DocumentArrowDownIcon className="" />
+										Copy UUID
+									</span>
+								</Menu.Item>
+								<Menu.Item>
+									<a
+										href={"https://google.com"}
+										className={
+											"flex flex-row align-baseline h-8 p-1 gap-2 text-slate-800 text-sm px-2 hover:bg-slate-200"
+										}
+									>
+										<FolderArrowDownIcon className="" />
+										Download Skin
+									</a>
+								</Menu.Item>
+								<Menu.Item>
+									<a
+										href={"https://drive.google.com"}
+										className={
+											"flex flex-row align-baseline h-8 p-1 gap-2 text-slate-800 text-sm px-2 hover:bg-slate-200"
+										}
+									>
+										<FolderArrowDownIcon className="" />
+										Download Skin Head
 									</a>
 								</Menu.Item>
 							</Menu.Items>
