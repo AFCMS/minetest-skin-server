b26b16a3386d00ae7fafb4cd778b19a2496000eb
AFCMS <afcm.contact@gmail.com>
2022-12-28 13:55:08 +0100

    [backend] Fetch user data from auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,8 +1,9 @@
 package middleware
 
 import (
-	"log"
+	"minetest-skin-server/database"
 	"minetest-skin-server/utils"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v3"
@@ -13,9 +14,30 @@ func AuthHandler() fiber.Handler {
 	return jwtware.New(jwtware.Config{
 		//Claims: jwt.RegisteredClaims{},
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			log.Println(c.Locals("user"))
-			return err
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Invalid JWT", "data": err})
 		},
+		SuccessHandler: func(c *fiber.Ctx) error {
+			token := c.Locals("user_jwt").(*jwt.Token)
+
+			// TODO: use RegisteredClaims instead of MapClaims
+			claims := token.Claims.(jwt.MapClaims)
+
+			cs, err := strconv.ParseInt(claims["iss"].(string), 10, 0)
+
+			if err != nil {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Invalid JWT"})
+			}
+
+			user_account, err := database.AccountFromID(int(cs))
+
+			if err != nil {
+				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "User not found"})
+			}
+
+			c.Locals("user", user_account)
+			return c.Next()
+		},
+		ContextKey:    "user_jwt",
 		SigningKey:    utils.ConfigJWTSecret,
 		SigningMethod: jwt.SigningMethodHS256.Name,
 		TokenLookup:   "cookie:jwt",
--- routes/account_user.go
+++ routes/account_user.go
@@ -1,33 +1,13 @@
 package routes
 
 import (
-	"minetest-skin-server/database"
 	"minetest-skin-server/models"
-	"strconv"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v4"
 )
 
 func AccountUser(c *fiber.Ctx) error {
-	token := c.Locals("user").(*jwt.Token)
-
-	// TODO: use RegisteredClaims instead of MapClaims
-	claims := token.Claims.(jwt.MapClaims)
-
-	var user models.Account
-
-	cs, err := strconv.ParseInt(claims["iss"].(string), 10, 0)
-
-	if err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "User not found"})
-	}
-
-	user, err = database.AccountFromID(int(cs))
-
-	if err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "User not found"})
-	}
+	user := c.Locals("user").(models.Account)
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"id":               user.ID,
