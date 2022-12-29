8c5bf1126dd8a2042a2f81641a62afb29922c6d4
AFCMS <afcm.contact@gmail.com>
2022-12-30 00:06:17 +0100

    [frontend] Login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import axios from "axios";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
+import { AuthStateIsAuthenticated } from "../../states/auth";
+import ApiUrls from "../../services/api_urls";
 
 function Login() {
 	const [email, setEmail] = useState("");
@@ -7,8 +12,11 @@ function Login() {
 
 	const [canSubmit, setCanSubmit] = useState(false);
 
+	const [loading, setLoading] = useState(false);
+	const [err, setErr] = useState(null);
+
 	useEffect(() => {
-		if (email === "" && password === "") {
+		if (email === "" || password === "") {
 			setCanSubmit(false);
 		} else {
 			setCanSubmit(true);
@@ -17,54 +25,96 @@ function Login() {
 
 	const navigate = useNavigate();
 
+	// Navigate to homepage if already authenticated
+	if (useRecoilValue(AuthStateIsAuthenticated)) {
+		return <Navigate to="/" />;
+	}
+
+	/**
+	 * @param {React.FormEvent<HTMLFormElement>} e
+	 */
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setLoading(true);
+		await axios
+			.post(ApiUrls.AccountLogin, {
+				email: email,
+				password: password,
+			})
+			.then((r) => {
+				//console.log(r);
+				setLoading(false);
+				navigate("/");
+			})
+			.catch((e) => {
+				//console.log(e);
+				setLoading(false);
+				setErr(e);
+			});
+	}
+
 	return (
-		<div className="justify-center align-middle flex">
-			<div className="flex flex-col max-w-md p-6 gap-4 m-10 app-pannel">
+		<div className="flex justify-center align-middle">
+			<div className="app-pannel m-10 flex max-w-md flex-col p-6">
 				<h1 className="panel-text-heading">Login</h1>
-				<label className="block">
-					<span className="text-slate-800">Email</span>
-					<input
-						type="email"
-						name="email"
-						id="email"
-						placeholder="Ex: someone@gmail.com"
-						className="form"
-						value={email}
-						onChange={(e) => {
-							setEmail(e.target.value);
-						}}
-					/>
-				</label>
-				<label className="block">
-					<span className="text-slate-800">Password</span>
-					<input
-						type="password"
-						name="password"
-						id="password"
-						placeholder="SuperStrongPassword"
-						className="form"
-						value={password}
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
-					/>
-				</label>
-				<div className="flex gap-4 mt-4">
-					<button
-						className="button-secondary w-full"
-						onClick={(e) => {
-							navigate("/register");
-						}}
-					>
-						Register
-					</button>
-					<button
-						className="button-primary w-full disabled:bg-black disabled:cursor-not-allowed"
-						disabled={!canSubmit}
-					>
-						Login
-					</button>
-				</div>
+				<form
+					className="mt-4 flex flex-col gap-4"
+					onSubmit={handleSubmit}
+				>
+					<label className="block">
+						<span className="text-slate-800">Email</span>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							placeholder="someone@gmail.com"
+							className="form"
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						/>
+					</label>
+					<label className="block">
+						<span className="text-slate-800">Password</span>
+						<input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="SuperStrongPassword"
+							className="form"
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
+						/>
+					</label>
+					{err !== null ? (
+						<div className="mt-4 flex flex-row items-center gap-2 text-red-600">
+							<ExclamationTriangleIcon className="h-8 w-8" />
+							{err.message}
+						</div>
+					) : undefined}
+					<div className="mt-4 flex gap-4">
+						<button
+							className="button-secondary w-full"
+							onClick={(e) => {
+								navigate("/register");
+							}}
+						>
+							Register
+						</button>
+						<button
+							className={`button-primary w-full disabled:cursor-not-allowed disabled:bg-black ${
+								loading ? "cursor-wait" : null
+							}`}
+							type="submit"
+							disabled={!canSubmit}
+						>
+							Login
+						</button>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
