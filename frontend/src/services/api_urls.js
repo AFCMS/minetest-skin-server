7f4b75c5cb0d21fdf7b88f76a244f07efd273e6f
AFCMS <afcm.contact@gmail.com>
2022-12-29 20:34:53 +0100

    [frontend] Store API routes

---
+++ frontend/src/services/api_urls.js
@@ -0,0 +1,16 @@
+import isDev from "./is_dev";
+
+const prefix = isDev() ? "http://localhost:8080" : "";
+
+const ApiUrls = {
+	Info: prefix + "/api/info",
+	AccountLogin: prefix + "/api/account/login",
+	AccountLogout: prefix + "/api/account/logout",
+	AccountRegister: prefix + "/api/account/register",
+	AccountUser: prefix + "/api/account/user",
+	SkinList: prefix + "/api/skin/list",
+	SkinCreate: prefix + "/api/skin/create",
+	SkinRecent: prefix + "/api/skin/recent",
+};
+
+export default ApiUrls;
