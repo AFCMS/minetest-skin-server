a86841ff7bb92bad849de3153022ae07233f5705
AFCMS <afcm.contact@gmail.com>
2023-01-01 01:29:16 +0100

    [backend] Add helper function to set permission level of an account

---
+++ database/account_set_permission.go
@@ -0,0 +1,12 @@
+package database
+
+import (
+	"minetest-skin-server/models"
+)
+
+func AccountSetPermission(a *models.Account, level int8) error {
+	if err := DB.Model(&a).Update("permission_level", level).Error; err != nil {
+		return err
+	}
+	return nil
+}
