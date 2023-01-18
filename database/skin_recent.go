c32318a75e3d413fcaec6a150f4db7353ae0c4fb
AFCMS <afcm.contact@gmail.com>
2023-01-18 11:11:46 +0100

    [backend] Add helper function to get recently published skins

---
+++ database/skin_recent.go
@@ -0,0 +1,13 @@
+package database
+
+import "minetest-skin-server/models"
+
+// Return the `count` recently published skins
+func SkinRecent(count uint8) ([]models.Skin, error) {
+	var results []models.Skin
+
+	if err := DB.Find(&results).Where("public = true").Order("created_at DESC").Limit(int(count)).Error; err != nil {
+		return results, err
+	}
+	return results, nil
+}
