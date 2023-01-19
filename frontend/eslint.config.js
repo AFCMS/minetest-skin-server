e5c626438b04d8e24d6cd16e053244ce7c3e154f
AFCMS <afcm.contact@gmail.com>
2023-01-19 16:11:29 +0100

    [config] Add ESLint config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"react/react-in-jsx-scope": "off",
+		"react/jsx-filename-extension": [
+			1,
+			{ extensions: [".js", ".jsx", ".tsx"] },
+		],
+	},
+};
