223fe62aaf7232d4df565564197b92c43a5f19ae
AFCMS <afcm.contact@gmail.com>
2023-01-22 00:18:56 +0100

    [backend] Use helper function to get recent skins

--- routes/skin_recent.go
+++ routes/skin_recent.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	"minetest-skin-server/database"
-	"minetest-skin-server/models"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 // TODO: cache result
 func SkinRecent(c *fiber.Ctx) error {
-	var results []models.Skin
-	if err := database.DB.Find(&results).Order("created_at DESC").Limit(10).Error; err != nil {
+	results, err := database.SkinRecent(10)
+
+	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
+
 	return c.Status(fiber.StatusOK).JSON(results)
 }
