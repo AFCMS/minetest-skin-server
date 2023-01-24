bb61fa9af045892093efd3bfb3fbbe586c101c01
AFCMS <afcm.contact@gmail.com>
2023-01-24 17:00:16 +0100

    [frontend] Enable plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,8 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/forms")],
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@headlessui/tailwindcss"),
+	],
 };
