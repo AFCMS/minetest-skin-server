19fa20d0c0b3caea2a8a64127c00df36a794084e
AFCMS <afcm.contact@gmail.com>
2024-02-22 17:45:53 +0100

    PostgreSQL database code support

--- database/database.go
+++ database/database.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"minetest-skin-server/models"
 	"minetest-skin-server/utils"
+	"os"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -14,33 +16,23 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	// Allow error logging only if debug enabled
-	var dblogger logger.Interface = logger.Default.LogMode(logger.Silent)
+	var dblogger = logger.Default.LogMode(logger.Silent)
 	if utils.ConfigDebugDatabase {
 		dblogger = logger.Default
 	}
 
 	var err error
-	DB, err = gorm.Open(sqlite.Open("database.db"), &gorm.Config{
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: dblogger,
 	})
-	//dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	//DB, err := gorm.Open(postgres.Open(fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=%d", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), 5432)), &gorm.Config{
-	//	Logger: logger.Default,
-	//})
 
 	if err != nil {
 		log.Fatalln("Failed to connect database")
 	}
 
-	if err := DB.Exec("PRAGMA foreign_keys = ON", nil).Error; err != nil {
-		log.Fatalln(err)
-	}
-
-	//defer DB.Close()
-
 	err = DB.AutoMigrate(&models.Account{}, &models.Skin{}, &models.SkinLike{})
 	if err != nil {
 		log.Fatalln("Failed to migrate database")
 	}
-	//db.Logger = logger.Default.LogMode(logger.Info)
 }
--- go.mod
+++ go.mod
@@ -9,10 +9,15 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.19.0
-	gorm.io/driver/sqlite v1.5.5
+	gorm.io/driver/postgres v1.5.6
 )
 
-require github.com/mattn/go-sqlite3 v1.14.17 // indirect
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
