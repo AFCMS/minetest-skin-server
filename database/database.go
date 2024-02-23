cbf47e42a436031dcb8167efcd6987f99ca449f1
AFCMS <afcm.contact@gmail.com>
2024-02-23 11:45:31 +0100

    Fix panic caused by `database.DB` being nil

--- database/database.go
+++ database/database.go
@@ -23,7 +23,7 @@ func ConnectDB() {
 
 	var err error
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: dblogger,
 	})
 
