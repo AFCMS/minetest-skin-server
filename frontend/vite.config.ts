3f7d56eac36258f856f6363ab365663d64bf93ce
AFCMS <afcm.contact@gmail.com>
2024-03-07 19:46:36 +0100

    Stricter Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
 
+// https://vitejs.dev/config/
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-	plugins: [react()],
-	assetsInclude: ["**/*.gltf"],
-});
+    build: {
+        manifest: true,
+        modulePreload: {
+            polyfill: false
+        }
+    },
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
+    },
+    plugins: [react()],
+    assetsInclude: ["**/*.gltf"],
+})
