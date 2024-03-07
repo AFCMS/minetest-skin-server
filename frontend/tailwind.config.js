14b787401d07c362b72a3cfecf3b10cde3bae2b0
AFCMS <afcm.contact@gmail.com>
2024-03-07 19:47:13 +0100

    Fix config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// noinspection JSUnusedGlobalSymbols
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
+}
+
