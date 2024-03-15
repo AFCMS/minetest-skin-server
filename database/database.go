29d70213e249923bd0ff3632aeb3bc23427ade1b
AFCMS <afcm.contact@gmail.com>
2024-03-15 10:13:54 +0100

    Rename NPM script, add production Compose file

--- database/database.go
+++ database/database.go
@@ -21,7 +21,13 @@ func ConnectDB() {
 	}
 
 	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("MT_SKIN_SERVER_DB_HOST"),
+		os.Getenv("MT_SKIN_SERVER_DB_USER"),
+		os.Getenv("MT_SKIN_SERVER_DB_PASSWORD"),
+		os.Getenv("MT_SKIN_SERVER_DB_NAME"),
+		os.Getenv("MT_SKIN_SERVER_DB_PORT"),
+	)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: dblogger,
 	})
