f4e0dbf658f874f4a8d70e241fd8c995ba7d56b4
AFCMS <afcm.contact@gmail.com>
2024-03-25 13:49:45 +0100

    Add supported OAuth providers to info API route

--- routes/info.go
+++ routes/info.go
@@ -1,9 +1,10 @@
 package routes
 
 import (
-	"minetest-skin-server/database"
-
 	"github.com/gofiber/fiber/v3"
+
+	"minetest-skin-server/database"
+	"minetest-skin-server/utils"
 )
 
 // TODO: include user count
@@ -19,9 +20,18 @@ func Info(c fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
+	// Return supported OAuth providers
+	// Used by the frontend to determine which OAuth buttons to display
+	var supportedOAuthProviders []string
+
+	if utils.ConfigOAuthContentDB {
+		supportedOAuthProviders = append(supportedOAuthProviders, "contentdb")
+	}
+
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"version":       "1.0",
-		"account_count": accountCount,
-		"skin_count":    skinCount,
+		"version":                   "1.0",
+		"account_count":             accountCount,
+		"skin_count":                skinCount,
+		"supported_oauth_providers": supportedOAuthProviders,
 	})
 }
