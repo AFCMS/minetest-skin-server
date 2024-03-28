26de8406907bf3c5777c826beeda02da0cea8ffb
AFCMS <afcm.contact@gmail.com>
2024-03-28 11:14:05 +0100

    Add more API types

--- frontend/src/services/api_types.d.ts
+++ frontend/src/services/api_types.d.ts
@@ -1,21 +1,37 @@
 declare namespace ApiTypes {
-	type UserResponse = {
-		id: number;
-		name: string;
-		email: string;
-		permission_level: 1 | 2 | 3 | 4;
-		banned?: boolean;
-		ban_reason?: string;
-		created_at: string;
-		last_connection: string;
-	};
+    type UserResponse = {
+        id: number;
+        name: string;
+        email: string;
+        permission_level: 1 | 2 | 3 | 4;
+        banned?: boolean;
+        ban_reason?: string;
+        created_at: string;
+        last_connection: string;
+    };
 
-	type SkinResponse = {
-		uuid: string;
-		description: string;
-		public?: boolean;
-		approved?: boolean;
-		owner_id: number;
-		creation_date: string;
-	};
+    type SkinResponse = {
+        uuid: string;
+        description: string;
+        public?: boolean;
+        approved?: boolean;
+        owner_id: number;
+        creation_date: string;
+    };
+
+    type AccountUserResponse = {
+        id: number;
+        username: string;
+        permission_level: 1 | 2 | 3 | 4;
+        cdb_username: string;
+    };
+
+    type InfoProviderTypes = "contentdb" | "github" | "codeberg" | "discord";
+
+    type InfoResponse = {
+        account_count: number;
+        skin_count: number;
+        version: string;
+        supported_oauth_providers: InfoProviderTypes[];
+    };
 }
