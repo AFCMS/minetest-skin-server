c2a68d5a57370d242cd1566c638ed8be5fda4572
AFCMS <afcm.contact@gmail.com>
2024-03-28 10:57:37 +0100

    Add API URLs for providers to frontend

--- frontend/src/services/api_urls.ts
+++ frontend/src/services/api_urls.ts
@@ -5,6 +5,10 @@ const ApiUrls = {
     AccountLogout: "/api/account/logout",
     AccountRegister: "/api/account/register",
     AccountUser: "/api/account/user",
+    AccountProviderCDB: "/api/account/providers/contentdb",
+    AccountProviderGitHub: "/api/account/providers/github",
+    AccountProviderCodeberg: "/api/account/providers/codeberg",
+    AccountProviderDiscord: "/api/account/providers/discord",
     Skin: "/api/skin/skin/", // +uuid
     SkinList: "/api/skin/list",
     SkinCreate: "/api/skin/create",
